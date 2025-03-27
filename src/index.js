e344a5976e06b2207b030c26ce5f8dddf6663db3
Eugene Kapombe <kapombek@gmail.com>
2025-03-28 00:46:25 +0300

    Grabbed DOM elements into variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-   //DOM Tree initialized... 
+    //DOM Tree initialized...
+    //variables declaration for input elements
+    const searchButton = document.getElementById('findBtn');
+    const userEntry = document.getElementById('userEntry');
+    const resultDiv = document.getElementById('results');
+    const favoritesDiv = document.getElementById('favorites');
+    //set empty array for favorites
+    let favoriteAnime = [];
+
+    
 });
